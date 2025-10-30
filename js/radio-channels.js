9e0b717a75330aeca90aee7dbca14e5e9713bf78
Dinesh <dinesh@exceleron.com>
2025-10-30 15:44:31 +0530

    Code cleanup

---
+++ js/radio-channels.js
@@ -0,0 +1,19 @@
+// Tamil Radio Channels - Working links only
+// Last updated: 2025-10-30
+// Only includes channels that passed connectivity tests
+
+var objMain = {
+	"Channels": {
+		"A9 FM": "http://195.154.217.103:8175/;stream.mp3",
+		"CTBC": "http://ctbcfmradio.primcast.com:4086/;",
+		"FTC": "http://167.114.131.90:5750/;?pl",
+		"Devotional": "http://curiosity.shoutca.st:9655/stream",
+		"Iyalisai": "http://andromeda.shoutca.st:9350/stream",
+		"Kuyil": "http://live.tamilkuyilradio.com:8095/;?pl",
+		"Lanka Sri": "http://media2.lankasri.fm/;?p",
+		"Jaffna": "http://jtrfm.ddnss.de:8000/;stream.mp3",
+		"StarMusiq": "http://andromeda.shoutca.st:9350/acc64",
+		"Thendral": "http://sc1.sytes.net:9116/;stream.mp3",
+		"US TamilFM": "http://streaming.radio.co/s0cfc93915/listen"
+	}
+};
